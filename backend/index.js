815c64671b458840c966c0c7e4bb4c584a07411e
Vishal Panwar <vishal4panwar@gmail.com>
2025-06-24 23:42:58 +0530

    connect db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config({ path: "./backend/config/.env" });
 const PORT = process.env.PORT || 5000;
@@ -7,6 +8,20 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use("/public", express.static("public"));
-app.listen(PORT, () => {
-  console.log("Server started on port ", PORT);
-});
+// app.listen(PORT, () => {
+//   console.log("Server started on port ", PORT);
+// });
+
+connectMongoDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`app is not able to connect :: ${error}`);
+      throw error;
+    });
+    app.listen(PORT || 3000, () => {
+      console.log(`app is listening on port :: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`index.js :: connectDB connection failed  :: ${error}`);
+  });
