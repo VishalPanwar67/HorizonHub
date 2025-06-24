7beceb91697ae3f20e5ca238b7fc29bbc31f4e50
Vishal Panwar <vishal4panwar@gmail.com>
2025-06-25 01:34:13 +0530

    fix admin backend error

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,30 +6,36 @@ import {
   Subscription,
 } from "../models/index.models.js";
 import { asyncHandler } from "../utils/index.utils.js";
+import cloudinary from "cloudinary";
 
-const addProject = asyncHandler(async (req, res, next) => {
-  const { name, description } = req.body;
-  const imageFile = req.file;
+const addProject = asyncHandler(async (req, res) => {
+  const { name, description, imageUrl } = req.body;
+  const imageFile = imageUrl;
 
+  if (!name || !description) {
+    throw new ApiError(400, "Name and description are required");
+  }
   if (!imageFile) {
     throw new ApiError(400, "Image file is required");
   }
 
   //   const croppedImageBuffer = await cropImage(imageFile.buffer);
-  const croppedImageBuffer = imageFile;
-  const uploadResult = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream({ folder: "admin_uploads" }, (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      })
-      .end(croppedImageBuffer);
-  });
+  let croppedImageBuffer = imageFile;
+
+  if (croppedImageBuffer) {
+    const uploadResponse = await cloudinary.uploader.upload(
+      croppedImageBuffer,
+      {
+        folder: "admin_uploads",
+      }
+    );
+    croppedImageBuffer = uploadResponse.secure_url;
+  }
 
   const project = await Project.create({
     name,
     description,
-    imageUrl: uploadResult.secure_url,
+    imageUrl: croppedImageBuffer,
   });
 
   res
@@ -38,29 +44,30 @@ const addProject = asyncHandler(async (req, res, next) => {
 });
 
 const addClient = asyncHandler(async (req, res, next) => {
-  const { name, description, designation } = req.body;
-  const imageFile = req.file;
+  const { name, description, designation, imageUrl } = req.body;
+  const imageFile = imageUrl;
 
   if (!imageFile) {
     throw new ApiError(400, "Image file is required");
   }
 
   //   const croppedImageBuffer = await cropImage(imageFile.buffer);
-  const croppedImageBuffer = imageFile;
-  const uploadResult = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream({ folder: "admin_uploads" }, (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      })
-      .end(croppedImageBuffer);
-  });
+  let croppedImageBuffer = imageFile;
+  if (croppedImageBuffer) {
+    const uploadResponse = await cloudinary.uploader.upload(
+      croppedImageBuffer,
+      {
+        folder: "admin_uploads",
+      }
+    );
+    croppedImageBuffer = uploadResponse.secure_url;
+  }
 
   const client = await Client.create({
     name,
     description,
     designation,
-    imageUrl: uploadResult.secure_url,
+    imageUrl: croppedImageBuffer,
   });
 
   res
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cloudinary from "cloudinary";
 import { adminRoutes } from "./routes/index.routes.js";
 
 dotenv.config({ path: "./backend/config/.env" });
@@ -14,8 +17,10 @@ cloudinary.config({
 });
 
 const app = express();
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(express.json());
 app.use("/api/admin", adminRoutes);
 
 app.use("/public", express.static("public"));
